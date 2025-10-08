e65faca4df945e877e709dd91ec1e41ccd4d88c9
Shreya Sanjay Khaire <shreyakhaire33@gmail.com>
2025-10-08 10:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (40.07%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+// Shreya
+    bool wordPattern(string pattern, string s) 
+    {
+        unordered_map<char, int> charToIndex;
+        unordered_map<string, int> wordToIndex;
+
+        stringstream ss(s);
+        string token;
+
+        int countTokens = 0;
+        int i = 0;
+        int n = pattern.size();
+
+        while (ss >> token) 
+        {
+            countTokens++;
+
+            if (i == n || charToIndex[pattern[i]] != wordToIndex[token])
+                return false;
+
+            charToIndex[pattern[i]] = i + 1;
+            wordToIndex[token] = i + 1;
+
+            i++;
+        }
+
+        if (countTokens != n || i != n)
+            return false;
+
+        return true;
+    }
+};
