b851197c24ce3805476b628fd645fabda32b0d8e
Shreya Sanjay Khaire <shreyakhaire33@gmail.com>
2025-10-21 23:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (64.77%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    // Shreya
+public:
+// Shreya
+    int findMin(vector<int>& nums) {
+      int left = 0, right = nums.size() - 1;
+
+        while (left < right) 
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) 
+            {
+                left = mid + 1;
+            } 
+            else if (nums[mid] < nums[right]) 
+            {
+                right = mid;
+            } 
+            else 
+            {
+                right--;
+            }
+        }
+        return nums[left];   
+    }
+};
